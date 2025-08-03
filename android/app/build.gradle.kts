c545d53636ea017bf7eb043a8ad61fb5f4c9853f
Dilan <dilan.hansike@gmail.com>
2025-08-03 13:05:47 -0400

    implemented the chat systems

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 android {
     namespace = "com.example.laundry"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
